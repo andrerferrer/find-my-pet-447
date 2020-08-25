28df8f74495992cb94ad7f18aee6c4278e809dd3
Andre Ferrer <andrerferrer@hotmail.com>
2020-08-25 17:46:32 -0300

    Generate the controller and the views with some trash code

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,23 @@
+class PetsController < ApplicationController
+  def index
+    @pets = Pet.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # see all
+  # see all - index
   # see one
   # create
   # update
